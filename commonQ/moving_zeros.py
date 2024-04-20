d7d900aaa76faf4d45a0c48aedcfa040fcb22e23
Thomaskutty Reji <thomaskutty13.reji@gmail.com>
2024-04-21 00:52:33 +0530

    updated

--- commonQ/moving_zeros.py
+++ commonQ/moving_zeros.py
@@ -2,24 +2,18 @@
 # output : [1,3,12,0,0] ; maintain the order 
 
 def move_zeros(ar):
-    zero_count = ar.count(0)
-    array_length = len(ar)
-    i = 0
-    c=0
-    while i<=array_length-zero_count:
-        print(i,ar)
-        if ((ar[i] ==0) & (ar[i+1] !=0)):
-            ar[i],ar[i+1] = ar[i+1],ar[i]
+    i = 0 
+    j = 0 
+    while i<=len(ar):
+        if ar[i] !=0:
+            j = i
             i+=1
-            c+=1
 
-        elif ((ar[i] ==0) & (ar[i+1] ==0)):
-            ar[i],ar[i+2] = ar[i+2],ar[i]
+        elif ar[i] ==0:
+            ar[i],ar[j] = ar[j],ar[i]
             i+=1
-        else:
-            i+=1
-        
-    
+
+        print(ar)
 
     
 
